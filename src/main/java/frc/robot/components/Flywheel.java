dea3956a571ac8ced5584d52dfd9804db49eee92
RyanHir <4690732+RyanHir@users.noreply.github.com>
2020-02-01 13:31:06 -0600

    flywheel follow with assert to double check if follow works

--- src/main/java/frc/robot/components/Flywheel.java
+++ src/main/java/frc/robot/components/Flywheel.java
@@ -1,5 +1,6 @@
 package frc.robot.components;
 
+import com.ctre.phoenix.motorcontrol.InvertType;
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonFX;
 
 import frc.robot.Constants;
@@ -18,15 +19,17 @@ public class Flywheel extends Component<Robot> {
         flywheelLeft = new WPI_TalonFX(Constants.SHOOTER_FLYWHEEL_LEFT_ID);
         flywheelRight = new WPI_TalonFX(Constants.SHOOTER_FLYWHEEL_RIGHT_ID);
 
+        flywheelRight.follow(flywheelRight);
+
         // Set Inverted Mode
         flywheelLeft.setInverted(Constants.SHOOTER_FLYWHEEL_REVERSE);
-        flywheelRight.setInverted(!Constants.SHOOTER_FLYWHEEL_REVERSE); // Inverted via "!"
+        flywheelRight.setInverted(InvertType.OpposeMaster); // Inverted via "!"
 
-        flywheelRight.follow(flywheelLeft);
+        assert (flywheelLeft.getInverted() == !flywheelRight.getInverted());
 
         // Set Neutral Mode
         flywheelLeft.setNeutralMode(Constants.SHOOTER_FLYWHEEL_NEUTRALMODE);
-        // flywheelRight.setNeutralMode(Constants.SHOOTER_FLYWHEEL_NEUTRALMODE);
+        flywheelRight.setNeutralMode(Constants.SHOOTER_FLYWHEEL_NEUTRALMODE);
 
         flywheelLeft.configClosedloopRamp(Constants.SHOOTER_FLYWHEEL_RAMPING_SPEED);
         // flywheelRight.configClosedloopRamp(Constants.SHOOTER_FLYWHEEL_RAMPING_SPEED);
@@ -44,11 +47,11 @@ public class Flywheel extends Component<Robot> {
     public void periodic(Robot robot) {
         if (flywheelToggle.getState()) {
             flywheelLeft.set(shooterSpeed);
-            flywheelRight.set(shooterSpeed);
         } else {
             flywheelLeft.set(0);
-            flywheelRight.set(0);
         }
+
+        assert (flywheelLeft.get() == flywheelRight.get());
     }
 
     @Override
