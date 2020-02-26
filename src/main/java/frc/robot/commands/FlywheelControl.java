1b167767c2ba401f6ddb6a139fa05c2e0d93d8c4
RyanHir <4690732+RyanHir@users.noreply.github.com>
2020-02-25 20:23:00 -0600

    Misc

--- src/main/java/frc/robot/commands/FlywheelControl.java
+++ src/main/java/frc/robot/commands/FlywheelControl.java
@@ -6,10 +6,16 @@ import frc.robot.subsystems.Flywheel;
 public class FlywheelControl extends CommandBase {
     private final Flywheel flywheel;
     private final Flywheel.Speeds speed;
+    private final boolean stopOnDone;
 
     public FlywheelControl(Flywheel flywheel, Flywheel.Speeds speed) {
+        this(flywheel, speed, true);
+    }
+
+    public FlywheelControl(Flywheel flywheel, Flywheel.Speeds speed, boolean stopOnDone) {
         this.flywheel = flywheel;
         this.speed = speed;
+        this.stopOnDone = stopOnDone;
 
         addRequirements(flywheel);
     }
@@ -26,6 +32,7 @@ public class FlywheelControl extends CommandBase {
 
     @Override
     public void end(boolean interrupted) {
-        flywheel.set(Flywheel.Speeds.OFF);
+        if (stopOnDone)
+            flywheel.set(Flywheel.Speeds.OFF);
     }
 }
--- src/main/java/frc/robot/commands/StowSuperCommand.java
+++ src/main/java/frc/robot/commands/StowSuperCommand.java
@@ -6,8 +6,9 @@ import frc.robot.subsystems.*;
 public class StowSuperCommand extends SequentialCommandGroup {
 	public StowSuperCommand(Intake intake, Hopper hopper, Feeder feeder, Flywheel flywheel, Elevator elevator, Balancer balancer) {
 		addCommands(
-			new PivotCommand(intake, Intake.POSITION.UP) //,
-			// new ElevatorCommand(elevator, ClimberState.TOP) // FIXME
+			new PivotCommand(intake, Intake.POSITION.UP),
+			// new ElevatorCommand(elevator, ClimberState.TOP),
+			new FlywheelControl(flywheel, Flywheel.Speeds.OFF, true)
 		);
 
 		addRequirements(intake, hopper, feeder, flywheel, balancer);
