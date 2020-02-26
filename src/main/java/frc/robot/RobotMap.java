a1b6ccdafe9e76a4a86cc7d1cd01d0706d4bce81
5431-Programming <programming@frc5431.com>
2020-02-25 18:46:57 -0600

    Format Robot Map

--- src/main/java/frc/robot/RobotMap.java
+++ src/main/java/frc/robot/RobotMap.java
@@ -26,89 +26,99 @@ import frc.team5431.titan.core.joysticks.Xbox;
 import frc.team5431.titan.core.vision.Limelight;
 
 public class RobotMap {
-    private final Balancer balancer = new Balancer();
-    private final Drivebase drivebase = new Drivebase();
-    private final Elevator elevator = new Elevator();
-    private final Feeder feeder = new Feeder();
-    private final Flywheel flywheel = new Flywheel();
-    private final Hopper hopper = new Hopper();
-    private final Intake intake = new Intake();
+        private final Balancer balancer = new Balancer();
+        private final Drivebase drivebase = new Drivebase();
+        private final Elevator elevator = new Elevator();
+        private final Feeder feeder = new Feeder();
+        private final Flywheel flywheel = new Flywheel();
+        private final Hopper hopper = new Hopper();
+        private final Intake intake = new Intake();
 
-    private final Xbox driver = new Xbox(0);
-    private final LogitechExtreme3D operator = new LogitechExtreme3D(1);
+        private final Xbox driver = new Xbox(0);
+        private final LogitechExtreme3D operator = new LogitechExtreme3D(1);
 
-    private final Limelight limelight = new Limelight(Constants.VISION_FRONT_LIMELIGHT);
+        private final Limelight limelight = new Limelight(Constants.VISION_FRONT_LIMELIGHT);
 
-    private static enum Path {
+        private static enum Path {
 
-    };
+        };
 
-    SendableChooser<Path> chooser = new SendableChooser<>();
+        SendableChooser<Path> chooser = new SendableChooser<>();
 
-    public RobotMap() {
+        public RobotMap() {
 
-        bindKeys();
+                bindKeys();
 
-        // drivebase.setDefaultCommand(new DefaultDrive(drivebase, () -> driver.getRawAxis(Xbox.Axis.LEFT_Y),
-        //         () -> driver.getRawAxis(Xbox.Axis.LEFT_X)));
-    }
-
-    private void bindKeys() {
-        {
-            new JoystickButton(driver, Xbox.Button.BUMPER_R.ordinal() + 1)
-                    .toggleWhenPressed(new FlywheelControl(flywheel, Flywheel.Speeds.FULL), true);
-            new JoystickButton(driver, Xbox.Button.X.ordinal() + 1).whenPressed(new BalancerCommand(balancer, true));
-        //     new JoystickButton(driver, Xbox.Button.X.ordinal() + 1).whenPressed(new BalancerCommand(balancer, false));
         }
 
-        {
-        //     new JoystickButton(operator, LogitechExtreme3D.Button.ELEVEN.ordinal() + 1)
-        //             .whenPressed(new Targetor(drivebase, limelight));
-        }
-    }
+        private void bindKeys() {
+                // Driver Controls
+                {
+                        new JoystickButton(driver, Xbox.Button.BUMPER_R.ordinal() + 1)
+                                        .toggleWhenPressed(new FlywheelControl(flywheel, Flywheel.Speeds.FULL), true);
+                        new JoystickButton(driver, Xbox.Button.X.ordinal() + 1)
+                                        .whenPressed(new BalancerCommand(balancer, true));
+                        new JoystickButton(driver, Xbox.Button.X.ordinal() + 1)
+                                        .whenPressed(new BalancerCommand(balancer, false));
+                }
 
-    public Command getAutonomousCommand() {
+                // Operator Controls
+                {
+                        new JoystickButton(operator, LogitechExtreme3D.Button.ELEVEN.ordinal() + 1)
+                                        .whenPressed(new Targetor(drivebase, limelight));
+                }
 
-        final var autoVoltageConstraint = new DifferentialDriveVoltageConstraint(
-                new SimpleMotorFeedforward(DriveConstants.ksVolts, DriveConstants.kvVoltSecondsPerMeter,
-                        DriveConstants.kaVoltSecondsSquaredPerMeter),
-                DriveConstants.kDriveKinematics, 10);
-
-        final TrajectoryConfig config = new TrajectoryConfig(AutoConstants.kMaxSpeedMetersPerSecond,
-                AutoConstants.kMaxAccelerationMetersPerSecondSquared)
-                        // Add kinematics to ensure max speed is actually obeyed
-                        .setKinematics(DriveConstants.kDriveKinematics)
-                        // Apply the voltage constraint
-                        .addConstraint(autoVoltageConstraint);
-
-        Trajectory trajectory = null;
-
-        switch (chooser.getSelected()) {
-        default:
-            trajectory = TrajectoryGenerator.generateTrajectory(
-                    // Start at the origin facing the +X direction
-                    new Pose2d(0, 0, new Rotation2d(0)),
-                    // Pass through these two interior waypoints, making an 's' curve path
-                    List.of(new Translation2d(1, 1), new Translation2d(2, -1)),
-                    // End 3 meters straight ahead of where we started, facing forward
-                    new Pose2d(3, 0, new Rotation2d(0)),
-                    // Pass config
-                    config);
+                // Default Commands
+                {
+                        drivebase.setDefaultCommand(
+                                        new DefaultDrive(drivebase, () -> driver.getRawAxis(Xbox.Axis.LEFT_Y),
+                                                        () -> driver.getRawAxis(Xbox.Axis.LEFT_X)));
+                }
         }
 
-        assert (trajectory != null);
+        public Command getAutonomousCommand() {
 
-        final RamseteCommand ramseteCommand = new RamseteCommand(trajectory, drivebase::getPose,
-                new RamseteController(AutoConstants.kRamseteB, AutoConstants.kRamseteZeta),
-                new SimpleMotorFeedforward(DriveConstants.ksVolts, DriveConstants.kvVoltSecondsPerMeter,
-                        DriveConstants.kaVoltSecondsSquaredPerMeter),
-                DriveConstants.kDriveKinematics, drivebase::getWheelSpeeds,
-                new PIDController(DriveConstants.kPDriveVel, 0, 0), new PIDController(DriveConstants.kPDriveVel, 0, 0),
-                // RamseteCommand passes volts to the callback
-                drivebase::tankDriveVolts, drivebase);
+                final var autoVoltageConstraint = new DifferentialDriveVoltageConstraint(
+                                new SimpleMotorFeedforward(DriveConstants.ksVolts, DriveConstants.kvVoltSecondsPerMeter,
+                                                DriveConstants.kaVoltSecondsSquaredPerMeter),
+                                DriveConstants.kDriveKinematics, 10);
 
-        return ramseteCommand.andThen(() -> drivebase.tankDriveVolts(0, 0));
+                final TrajectoryConfig config = new TrajectoryConfig(AutoConstants.kMaxSpeedMetersPerSecond,
+                                AutoConstants.kMaxAccelerationMetersPerSecondSquared)
+                                                // Add kinematics to ensure max speed is actually obeyed
+                                                .setKinematics(DriveConstants.kDriveKinematics)
+                                                // Apply the voltage constraint
+                                                .addConstraint(autoVoltageConstraint);
 
-        // return chooser.getSelected();
-    }
+                Trajectory trajectory = null;
+
+                switch (chooser.getSelected()) {
+                default:
+                        trajectory = TrajectoryGenerator.generateTrajectory(
+                                        // Start at the origin facing the +X direction
+                                        new Pose2d(0, 0, new Rotation2d(0)),
+                                        // Pass through these two interior waypoints, making an 's' curve path
+                                        List.of(new Translation2d(1, 1), new Translation2d(2, -1)),
+                                        // End 3 meters straight ahead of where we started, facing forward
+                                        new Pose2d(3, 0, new Rotation2d(0)),
+                                        // Pass config
+                                        config);
+                }
+
+                assert (trajectory != null);
+
+                final RamseteCommand ramseteCommand = new RamseteCommand(trajectory, drivebase::getPose,
+                                new RamseteController(AutoConstants.kRamseteB, AutoConstants.kRamseteZeta),
+                                new SimpleMotorFeedforward(DriveConstants.ksVolts, DriveConstants.kvVoltSecondsPerMeter,
+                                                DriveConstants.kaVoltSecondsSquaredPerMeter),
+                                DriveConstants.kDriveKinematics, drivebase::getWheelSpeeds,
+                                new PIDController(DriveConstants.kPDriveVel, 0, 0),
+                                new PIDController(DriveConstants.kPDriveVel, 0, 0),
+                                // RamseteCommand passes volts to the callback
+                                drivebase::tankDriveVolts, drivebase);
+
+                return ramseteCommand.andThen(() -> drivebase.tankDriveVolts(0, 0));
+
+                // return chooser.getSelected();
+        }
 }
