e87c4fa18941d879a72c08aa90ccb3b1ecba58c5
Colin Wong <cwongmath@gmail.com>
2020-02-25 20:58:07 -0600

    Seq, not parallel

--- src/main/java/frc/robot/commands/ShootSuperCommand.java
+++ src/main/java/frc/robot/commands/ShootSuperCommand.java
@@ -1,10 +1,10 @@
 package frc.robot.commands;
 
-import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import frc.robot.subsystems.*;
 import frc.robot.subsystems.Flywheel.Speeds;
 
-public class ShootSuperCommand extends ParallelCommandGroup {
+public class ShootSuperCommand extends SequentialCommandGroup {
     public ShootSuperCommand(Intake intake, Hopper hopper, Feeder feeder, Flywheel flywheel) {
         addCommands(
             new FlywheelControl(flywheel, Speeds.FULL),
