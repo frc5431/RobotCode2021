509065c3119807eb93517c03386e193776d0ec8d
Colin Wong <cwongmath@gmail.com>
2020-02-25 20:01:09 -0600

    Floor and Stow States

--- src/main/java/frc/robot/commands/StowSuperCommand.java
+++ src/main/java/frc/robot/commands/StowSuperCommand.java
@@ -2,11 +2,15 @@ package frc.robot.commands;
 
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import frc.robot.subsystems.*;
+import frc.robot.util.states.ClimberState;
 
 public class StowSuperCommand extends SequentialCommandGroup {
-	public StowSuperCommand(Intake intake, Hopper hopper, Feeder feeder, Flywheel flywheel) {
+	public StowSuperCommand(Intake intake, Hopper hopper, Feeder feeder, Flywheel flywheel, Elevator elevator, Balancer balancer) {
 		addCommands(
-
+			new PivotCommand(intake, Intake.POSITION.UP) //,
+			// new ElevatorCommand(elevator, ClimberState.TOP) // FIXME
 		);
+
+		addRequirements(intake, hopper, feeder, flywheel, balancer);
 	}
 }
