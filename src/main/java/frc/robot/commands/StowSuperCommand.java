d893305ac4e0b1779048139df66f9315d888c961
Colin Wong <cwongmath@gmail.com>
2020-02-27 17:28:25 -0600

    Parallel Stow

--- src/main/java/frc/robot/commands/StowSuperCommand.java
+++ src/main/java/frc/robot/commands/StowSuperCommand.java
@@ -1,12 +1,12 @@
 package frc.robot.commands;
 
-import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
 import frc.robot.subsystems.*;
 
 /**
  * @author Ryan Hirasaki
  */
-public class StowSuperCommand extends SequentialCommandGroup {
+public class StowSuperCommand extends ParallelCommandGroup {
 	public StowSuperCommand(Intake intake, Hopper hopper, Feeder feeder, Flywheel flywheel, Elevator elevator, Balancer balancer, Pivot pivot) {
 		addCommands(
 			new PivotCommand(pivot, Pivot.POSITION.UP), // Brings the Intake Up
