e39bbb6010bf5daf1e8acc032e1442cf2ec37fbd
Ryan Hirasaki <ryan@localhost.localdomain>
2020-06-27 22:22:06 -0500

    Basic Constructors

--- src/main/java/frc/robot/auton/NineBall.java
+++ src/main/java/frc/robot/auton/NineBall.java
@@ -3,5 +3,9 @@ package frc.robot.auton;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 
 public class NineBall extends SequentialCommandGroup {
+	private final Drivebase drivebase;
 
+	public SixBall(Drivebase drivebase) {
+		this.drivebase = drivebase;
+	}
 }
--- src/main/java/frc/robot/auton/SixBall.java
+++ src/main/java/frc/robot/auton/SixBall.java
@@ -4,4 +4,10 @@ import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 
 public class SixBall extends SequentialCommandGroup {
 
+	private final Drivebase drivebase;
+
+	public SixBall(Drivebase drivebase) {
+		this.drivebase = drivebase;
+	}
+
 }
