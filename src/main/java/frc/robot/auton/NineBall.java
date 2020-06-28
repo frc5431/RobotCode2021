2dc099b03083e8efe324da7f8ae14cde8600607a
Ryan Hirasaki <ryan@localhost.localdomain>
2020-06-27 22:23:57 -0500

    I did not change class name on copy-paste :(

--- src/main/java/frc/robot/auton/NineBall.java
+++ src/main/java/frc/robot/auton/NineBall.java
@@ -5,7 +5,7 @@ import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 public class NineBall extends SequentialCommandGroup {
 	private final Drivebase drivebase;
 
-	public SixBall(Drivebase drivebase) {
+	public NineBall(Drivebase drivebase) {
 		this.drivebase = drivebase;
 	}
 }
