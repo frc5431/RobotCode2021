aa2dbece4ff8293a00ee6a8a5dc11e5e79a7da12
Colin Wong <cwongmath@gmail.com>
2021-02-01 17:16:05 -0600

    More formatting
    - Removes the unused method since it is reimplemented in WaitTillFlywheelAtSpeed
    - Spacing changes

--- src/main/java/frc/robot/commands/DefaultElevator.java
+++ src/main/java/frc/robot/commands/DefaultElevator.java
@@ -10,7 +10,6 @@ import frc.robot.subsystems.Elevator;
  * @author Ryan Hirasaki
  */
 public class DefaultElevator extends CommandBase {
-
     private final Elevator elevator;
     private final DoubleSupplier pow;
 
