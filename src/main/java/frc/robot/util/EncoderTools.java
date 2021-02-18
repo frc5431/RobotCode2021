5463097d0f4fc38b9134035b0aac8d362f4daba5
Ryan Hirasaki <ryanhirasaki@gmail.com>
2021-02-17 18:27:46 -0600

    Add wheel rotations to EncoderTools

--- src/main/java/frc/robot/util/EncoderTools.java
+++ src/main/java/frc/robot/util/EncoderTools.java
@@ -3,21 +3,27 @@ package frc.robot.util;
 import frc.robot.Constants;
 
 public class EncoderTools {
-    public static double ticksToMeters(double ticks) {
-        double wheel_rotation = (ticksToAxleRevolutions(ticks)) * Constants.GEAR_RATIO;
-        return wheel_rotation * Constants.WHEEL_CIRCUMFERENCE;
-    }
-
     public static double ticksToAxleRevolutions(double ticks) {
         return ticks / Constants.COUNTS_PER_REVOLUTION;
+    }
+    
+    public static double ticksToWheelRevolutions(double ticks) {
+        return ticksToAxleRevolutions(ticks) * Constants.GEAR_RATIO;
+    }
+    
+    public static double ticksToMeters(double ticks) {
+    return ticksToWheelRevolutions(ticks) * Constants.WHEEL_CIRCUMFERENCE;
     }
 
     public static double axleRevolutionsToTicks(double revolutions) {
         return revolutions * Constants.COUNTS_PER_REVOLUTION;
     }
+    
+    public static double wheelRevolutionsToTicks(double revolutions) {
+        return axleRevolutionsToTicks(revolutions / Constants.GEAR_RATIO);
+    }
 
     public static double metersToTicks(double meters) {
-        double wheel_rotations =  meters / Constants.WHEEL_CIRCUMFERENCE;
-        return axleRevolutionsToTicks(wheel_rotations / Constants.GEAR_RATIO);
+        return wheelRevolutionsToTicks(meters / Constants.WHEEL_CIRCUMFERENCE);
     }
 }
