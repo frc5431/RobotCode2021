a52c54d054a7e7c366da07ba86fb5d066b827c04
Ryan Hirasaki <ryanhirasaki@gmail.com>
2021-02-25 20:00:55 -0600

    Revert "Put em back in paths folder"
    This reverts commit 6405094d4e3dfc8e9fef2f8acf85ee9bd331826b.

--- src/main/java/frc/robot/util/GeneratePaths.java
+++ src/main/java/frc/robot/util/GeneratePaths.java
@@ -2,10 +2,12 @@ package frc.robot.util;
 
 import frc.robot.Constants;
 
+import java.io.File;
 import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 
+import edu.wpi.first.wpilibj.Filesystem;
 import frc.team5431.titan.core.misc.Logger;
 import frc.team5431.titan.pathfinder.PathLoader;
 import frc.team5431.titan.pathfinder.PathLoader.Status;
@@ -24,8 +26,7 @@ public class GeneratePaths {
 
     public static Map<String, PathLoader> generate() {
         var data = new HashMap<String, PathLoader>();
-        for (String kName : Constants.DRIVEBASE_PATHWEAVER_PATHS) {
-            kName = "paths/" + kName;
+        for (final String kName : Constants.DRIVEBASE_PATHWEAVER_PATHS) {
             PathLoader ldr = null;
             try {
                 ldr = build(kName);
